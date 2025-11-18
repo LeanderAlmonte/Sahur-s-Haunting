ce03524e3b8dcb05c22c14cdbcb3cc95641a4a30
Ibrahimcomputerscience <6257643@edu.vaniercollege.qc.ca>
2025-11-17 23:07:29 -0500

    Finished the table hiding spot + pickup paper+ time

---
+++ Assets/Scripts/HidingSystem.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class HidingSystem : MonoBehaviour
+{
+    public static HidingSystem Instance;
+
+    public GameObject hideOverlay; // assign from Canvas
+
+    private CharacterController controller;
+    private bool isHiding = false;
+    private Vector3 returnPosition;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+
+        controller = GetComponent<CharacterController>();
+        if (hideOverlay != null)
+            hideOverlay.SetActive(false);
+    }
+
+    public void ToggleHide(Transform hidePosition)
+    {
+        if (!isHiding)
+        {
+            // Enter hiding
+            returnPosition = transform.position;
+            if (controller != null) controller.enabled = false;
+
+            transform.position = hidePosition.position;
+
+            if (hideOverlay != null)
+                hideOverlay.SetActive(true);
+
+            isHiding = true;
+        }
+        else
+        {
+            // Exit hiding
+            transform.position = returnPosition;
+            if (controller != null) controller.enabled = true;
+
+            if (hideOverlay != null)
+                hideOverlay.SetActive(false);
+
+            isHiding = false;
+        }
+    }
+}
